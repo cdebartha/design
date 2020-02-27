d21716f159318596b3f09896a0da4416645b6110
Darshan <zala@iitk.ac.in>
2020-02-27 11:22:25 +0530

    bending analyasis with considering spar only

--- bendspar.m
+++ bendspar.m
@@ -1,22 +1,25 @@
+a=load('n63415-il.dat')
+z=-a(:,1)*c*3.28084*0.01;
+y= a(:,2)*c*3.28084*0.01;
 c=1.6190
-spar1_y1=0.1517*3.28084;
-spar1_y2=-0.08855*3.28084;
+spar1_ytop=0.1517*3.28084;
+spar1_ybot=-0.08855*3.28084;
 spar1_z=-0.3*c*3.28084;
-spar2_y1=0.09546*3.28084;
-spar2_y2=-0.031975*3.28084;
+spar2_ytop=0.09546*3.28084;
+spar2_ybot=-0.031975*3.28084;
 spar2_z=-0.7*c*3.28084;
-b1=(spar1_y1-spar1_y2);
-b2=(spar2_y1-spar2_y2);
+b1=(spar1_ytop-spar1_ybot);
+b2=(spar2_ytop-spar2_ybot);
 %% thicknesses and Modulus
 tw=0.004*3.28084;
 tf=tw/2;
 tsc=0.002*3.28084;
-hw1=(spar1_y1-spar1_y2-tf-tsc);
-hw2=(spar2_y1-spar2_y2-tf-tsc);
-E_al= 69*10^9*0.000145038;
-E_steel= 200*10^9*0.000145038;
+hw1=(spar1_ytop-spar1_ybot-2*(tf+tsc));
+hw2=(spar2_ytop-spar2_ybot-2*(tf+tsc));
+E_al= 1.5351*10^9;
+E_steel= 4.2*10^9;
 E_star=E_steel/E_al;
-ybar=[(spar1_y1+spar1_y2)/2,(spar2_y1+spar2_y2)/2];
+ybar=[(spar1_ytop+spar1_ybot)/2,(spar2_ytop+spar2_ybot)/2];
 zbar=[spar1_z,spar2_z];
 %% Areas of individual parts
 A1=[b1*tsc,b1*tsc,b1*tf,b1*tf,tw*hw1];
@@ -34,12 +37,21 @@ Iyy2=2*E_star*(tsc*b2^3/12)+2*(tf*b2^3/12)+ (hw2*tw^3/12);
 
 Iyy_total=Iyy1+ (sigmaEA(1)/E_al)*(zbar(1)-z_cent)^2 + Iyy2+ (sigmaEA(2)/E_al)*(zbar(2)-z_cent)^2;
 Izz_total=Izz1+ (sigmaEA(1)/E_al)*(ybar(1)-y_cent)^2 + Izz2+ (sigmaEA(2)/E_al)*(ybar(2)-y_cent)^2;    
-Iyz_total= (sigmaEA(1)/E_al)*(ybar(1)-y_cent)*(zbar(1)-z_cent) + (sigmaEA(2)/E_al)*(zbar(2)-z_cent)*(ybar(2)-y_cent);
+Iyz_total=((sigmaEA(1)/E_al)*(ybar(1)-y_cent)*(zbar(1)-z_cent) + (sigmaEA(2)/E_al)*(zbar(2)-z_cent)*(ybar(2)-y_cent));
 %% finding V_xx, W_xx
-A=[-E_star*Iyz_total,-E_star*Iyy_total ; E_star*Izz_total,E_star*Iyz_total];
+A=[-E_al*Iyz_total,-E_al*Iyy_total ; E_al*Izz_total,E_al*Iyz_total];
 M=[M_y_due_to_D(1),M_z_due_to_L(1)];
+%M=[855620,7376500]
 v=A\M';
 %% finding sigma_xx
-epsilon_xx= (y*v(1) +x*v(2));  %conversion from meter to feet
+epsilon_xx= (-(y-y_cent)*v(1)-(z-z_cent)*v(2));  %conversion from meter to feet
 sigma_xx= E_al*epsilon_xx;
-plot(sigma_xx)
+sigma_yield= [6804000,6804000]; %psi
+z1=[-5.3117,0];
+plot(-z,sigma_xx)
+hold on
+plot(-z1,sigma_yield);
+%% adding stigers
+z_stinger=[z(12),z(9),z(40),z(43)];
+y_stinger=[y(12),y(9),y(40),y(43)];
+
