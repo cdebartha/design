ad89e755affc5da97dceafb56a55f141fc90ef9f
vivekvrma <97vivekverma@gmail.com>
2020-05-31 12:32:04 +0530

    BucklingAnalysis.m improved the MOI

--- BucklingAnalysis.m
+++ BucklingAnalysis.m
@@ -2,12 +2,14 @@ clc,clear all
 %% spar buckling -->
 spar1_ytop = 0.1517*3.28084;
 spar1_ybot = -0.08855*3.28084;
-b1         = 0.4*(spar1_ytop - spar1_ybot);
+ycent      = 0.5*(spar1_ytop + spar1_ybot);
+
+b1         = 0.2*(spar1_ytop - spar1_ybot);
 half_span = 42.4944/2 ;
 %% Thicknesses and Modulus
-tw = 0.006*3.28084;     % ft
+tw = 0.008*3.28084;     % ft
 tf = tw/2;              % ft
-tsc = 7*0.001*3.28084;  % ft
+tsc = 0.01*3.28084;  % ft
 spr_cp_ar    = b1*tsc ; % ft^2
 spr_flng_ar  = b1*tf  ; % ft^2
 
@@ -17,21 +19,27 @@ hw1 = (spar1_ytop-spar1_ybot-2*(tf+tsc));
 E_al = 1.5351*10^9*32.1522; % lbf/ft^2 
 E_st = 4.2*10^9*32.1522;    % lbf/ft^2 
 
-I_zz_sprCap = 2*( ((b1*tsc^3/12))+(spr_cp_ar*((tsc+2*tf+hw1)/2)^2) ) ;
-I_zz_flange = 2*( ((b1*tf^3/12))+(spr_flng_ar*((tw/4)+(hw1/2))^2) )    ;
+I_zz_sprCap = ( ((b1*tsc^3/12))+(spr_cp_ar*((tsc+2*tf+hw1)/2)^2) ) ;
+I_zz_flange = ( ((b1*tf^3/12))+(spr_flng_ar*((tf/2)+(hw1/2))^2) )    ;
+
+I_Sigmazz_SC = 2*( ((b1*tsc^3/12))+(spr_cp_ar*((tsc+2*tf+hw1)/2)^2) ) ;
+I_Sigmazz_FL = 2*( ((b1*tf^3/12))+(spr_flng_ar*((tf/2)+(hw1/2))^2) )    ;
+
+I_buc_sprCap = ((b1*tsc^3/12)) ;
+I_buc_flange = ((b1*tf^3/12))    ;
 len_panel   = half_span/11 ;
 
-Mz = (.8/2)*1941400 ; % lbf*ft
+Mz = (.8/4)*1941400 ; % lbf*ft
 
 boom_area   = spr_cp_ar + spr_flng_ar ;
-sgma_xx_max = Mz*E_st*spar1_ytop/( E_st*I_zz_sprCap + E_al*I_zz_flange ) ;
+sgma_xx_max = Mz*E_st*(spar1_ytop-ycent)/( E_st*I_Sigmazz_SC + E_al*I_Sigmazz_FL ) ;
 %% for panel at root
 F_comp = sgma_xx_max*boom_area 
-P_crit = ( pi^2*(E_st*I_zz_sprCap + E_al*I_zz_flange) )/len_panel^2  
+P_crit = ( pi^2*(E_al*(I_buc_sprCap + I_buc_flange)) )/len_panel^2  
 
 %% for spar at root
 Fspar_comp = sgma_xx_max*boom_area 
-Pspar_crit = ( pi^2*(E_st*I_zz_sprCap + E_al*I_zz_flange) )/(1.12*half_span)^2  
+Pspar_crit = ( pi^2*(E_al*(I_zz_flange + I_zz_sprCap) ))/(1.12*half_span)^2  
 
 % %% for plate(web)
 % V_rootSh = 10580 ; % lbf shear force at root
